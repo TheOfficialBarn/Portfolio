e633b095adf55e293d5feb72f565bf09bcfc8930
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-09-01 15:31:38 -0500

    Added bg-fixed to prevent bg from scrolling with other elements (primarily for mobile)

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-[url("/White.jpeg")] bg-cover bg-center h-screen dark:text-white dark:bg-[url("/Black.jpeg")]',
+        'text-black bg-[url("/White.jpeg")] bg-cover bg-center bg-fixed h-screen dark:text-white dark:bg-[url("/Black.jpeg")]', //bg-fixed makes the background image not scroll with the page
         GeistSans.variable,
         GeistMono.variable
       )}
