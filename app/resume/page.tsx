212d2ec9b5375f44cd298573dd21fa7956ce85f0
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-09-02 15:22:34 -0500

    Updated Resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,8 +2,7 @@ export default function Page() {
     return(
         <section>
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">My Resume</h1>
-
-            <br/>
+            <p className="mb-4">Made with LaTeX</p>
 
             <div className="drop-shadow-2xl">
             <iframe src="/resume.pdf" className="w-full h-[745px] border-0" />
